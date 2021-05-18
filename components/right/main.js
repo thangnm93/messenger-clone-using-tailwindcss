c225c5a850b61a07a90d233afa8b0159c03faf84
Thắng NM <minhthang0403@gmail.com>
2021-05-18 18:20:01 +0700

    Update src

--- components/right/main.js
+++ components/right/main.js
@@ -6,10 +6,12 @@ import ChatDetail from "./chat-detail";
 export default function MainRight() {
   return (
     <div className="flex justify-between w-full">
-      <div className="bg-white dark:bg-bg-main border-r dark:border-bg-secondary w-3/4 relative">
-        <Header />
-        <Chat />
-        <Typing />
+      <div className="bg-white dark:bg-bg-main border-r dark:border-bg-secondary w-3/4">
+        <div className="relative">
+          <Header />
+          <Chat />
+          <Typing />
+        </div>
       </div>
       <ChatDetail />
     </div>
